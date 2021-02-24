7b9519cefdaaeeb66777a258c1f8e088ffc261c4
Claudia Osses <claudia.osses.mundaca@gmail.com>
2021-02-24 11:10:40 -0300

    - Se crea funcion de validacion de tarjetas - Se agrega funcion de maskify - Se modifico HTML - Se agregaron estilos en CSS - Se modificaron y validaron test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 import validator from './validator.js';
 
-console.log(validator);
+const regex = /[^0-9]/; 
+
+const botonImagen = () => {
+    let valor = document.querySelector(".cuadro").value;
+    if (validator.isValid(valor)) {
+        document.querySelector('.segundaPagina').style.display = "block";
+        document.querySelector('.primeraPagina').style.display = "none";
+        document.querySelector('.textoError').style.display = "none";
+        //document.querySelector('.mostrarTarjeta').innerText = "**** ".repeat(3) + valor.slice(12);
+        document.querySelector('.mostrarTarjeta').innerText = validator.maskify(valor);
+    } else {
+        document.querySelector(".tarInvalida").style.display = "block";
+    }
+}
+
+const segundoBoton = () => {
+    document.querySelector('.final').style.display = "block";
+    document.querySelector('.segundaPagina').style.display = "none";
+}
+
+const inicio = () => {
+    document.querySelector('.primeraPagina').style.display = "block";
+    document.querySelector('.segundaPagina').style.display = "none";
+    document.querySelector(".tarInvalida").style.display = "none";
+    resetearValores();
+}
+
+const volver = () => {
+    document.querySelector('.primeraPagina').style.display = "block";
+    document.querySelector('.final').style.display = "none";
+    document.querySelector(".tarInvalida").style.display = "none";
+    resetearValores();
+}
+
+const formatearNumero = () => {
+    const valor = document.querySelector('.cuadro').value;
+    document.querySelector('.cuadro').value = valor.replace(regex, '');
+
+}
+
+const resetearValores = () => {
+    document.querySelector('.cuadro').value = "";
+    document.querySelector('.cuadro2').value = "";
+}
+
+//Asignando evento onclick
+
+document.querySelector(".contenedor-imagen").addEventListener("click", botonImagen);
+//Al hacer click a la primera imagen, que ejecute la funcion segun corresponda, si la tarjeta es válida, pasar a segunda página
+document.querySelector(".contenedor2").addEventListener("click", segundoBoton);
+// Al hacer click a la segunda imagen, para confirmar el uso de la tarjeta, que nos envíe a la última página
+document.querySelector(".botonVolver").addEventListener("click", inicio);
+// Al presionar el botón volver, que me lleve a la página principal y que se reseteen los valores ingresados anteriormente
+document.querySelector(".finalizar").addEventListener("click", volver);
+// 
+document.querySelector(".cuadro").addEventListener("keyup", formatearNumero);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,55 @@
 const validator = {
-  // ...
-};
+  isValid(codigo) {
+    let suma = 0;
+    let valor = codigo.split("");
+
+    // Validar el valor del campo y devolver false si no se cumple la condicion
+    if (valor == 0) {
+      return false;
+    }
+
+    // Recorrer los valores y multiplicarlos por 2 cada dos posiciones
+    for (let i = 0; i < valor.length; i += 2) {
+      valor[i] = valor[i] * 2;
+    }
+
+    // Convertir todos los valores a string
+    for (let i = 0; i < valor.length; i++) {
+      valor[i] = valor[i].toString();
+    }
+
+    // Convierte valores de 2 dígitos en números y los suma
+    for (let i = 0; i < valor.length; i++) {
+      if (valor[i].length == 2) {
+        valor[i] = parseInt(valor[i].charAt(0)) + parseInt(valor[i].charAt(1));
+      }
+    }
+
+    // Cambia los valores de string a números, para poder sumarlos
+    for (let i = 0; i < valor.length; i++) {
+      valor[i] = parseInt(valor[i]);
+    }
+
+    // Suma de todos los valores en la variable "suma"
+    for (let i = 0; i < valor.length; i++) {
+      suma = suma + valor[i];
+    }
+
+    // Valida si el segundo dígito resultado de suma, termina en "0"
+    if (suma.toString().charAt(1) == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  maskify(codigo) {
+    if (codigo.length != 16) {
+      return false;
+    }
+    let enmas = "**** ".repeat(3) + codigo.slice(12);
+    return enmas; 
+  }
+}
+
 
 export default validator;
